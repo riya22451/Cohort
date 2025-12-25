c77c1f2d05f154b4e507995b27da1445cc7b0234
riya22451 <riyaaggarwal226@gmail.com>
2025-12-25 18:27:00 +0530

    connect the pgClient with neon database and also avoid sql injection

---
+++ postgres/src/index.ts
@@ -0,0 +1,20 @@
+import {Client} from "pg";
+const pgClient=new Client('postgresql://neondb_owner:npg_eu81vCZspISl@ep-orange-sea-a4wps4fp-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
+async function main(){
+await pgClient.connect()
+const response=await pgClient.query(`
+    CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+`)
+console.log(response)
+}
+main()
+
